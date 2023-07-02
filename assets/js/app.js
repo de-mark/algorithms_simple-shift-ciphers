23219b4e6a2db02eb1fb6f9dc30850c1ce033365
de-mark <data.engineer.mark@gmail.com>
2023-07-01 23:32:00 -0700

    Added super basic display for cipher

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+const resultArea = document.getElementById("res");
 const plainEntry = document.getElementById("usr-plain");
 const shiftEntry = document.getElementById("usr-shift");
 const encryptBtn = document.getElementById("btn-encrypt");
@@ -14,9 +15,24 @@ const encryptText = () => {
     let shiftBy = parseInt(shiftEntry.value);
     let textToEncrypt = plainEntry.value.toUpperCase().split('')
     let nextStep = textToEncrypt.map((p)=> p == " " ? " " : (alpha.indexOf(p) + shiftBy) % 26);
-    alert(nextStep);
     let shiftedCode = nextStep.map((p)=> p == " " ? " " : alpha[p]).join("");
-    alert(shiftedCode);
+    resultArea.innerHTML = `
+    <div>
+        <div>
+            <b>Plain text:</b> ${plainEntry.value}
+        </div>
+        <div>
+            <b>Shift By:</b> ${shiftBy}
+        </div>
+    </div>
+    <div>
+        <div>
+            <b>Code:</b> ${shiftedCode}
+        </div>
+    </div>
+    `
+    plainEntry.value = ""
+    shiftEntry.value = ""
 }
 
 
