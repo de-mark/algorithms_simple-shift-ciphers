1ec4b84bfde3b6f4145d9d827387e46adce4bc7b
de-mark <data.engineer.mark@gmail.com>
2023-07-01 23:21:41 -0700

    Fixed error with input and shift

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,24 @@
-const pEntry = document.getElementById("usr-p");
+const plainEntry = document.getElementById("usr-plain");
+const shiftEntry = document.getElementById("usr-shift");
+const encryptBtn = document.getElementById("btn-encrypt");
+
+const alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+               "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+               "U", "V", "W", "X", "Y", "Z"]
 
 const onlyNumberEntry = () =>  {
-    pEntry.value = pEntry.value.replace(/\D/g, "");
+    shiftEntry.value = shiftEntry.value.replace(/\D/g, "");
 }
 
-pEntry.addEventListener("change", onlyNumberEntry);
+const encryptText = () => {
+    let shiftBy = parseInt(shiftEntry.value);
+    let textToEncrypt = plainEntry.value.toUpperCase().split('')
+    let nextStep = textToEncrypt.map((p)=> p == " " ? " " : (alpha.indexOf(p) + shiftBy) % 26);
+    alert(nextStep);
+    let shiftedCode = nextStep.map((p)=> p == " " ? " " : alpha[p]).join("");
+    alert(shiftedCode);
+}
+
+
+shiftEntry.addEventListener("change", onlyNumberEntry);
+encryptBtn.addEventListener("click", encryptText);
