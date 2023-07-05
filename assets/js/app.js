bdd8e6998836fb740aef02401565b72cd86ec657
de-mark <data.engineer.mark@gmail.com>
2023-07-04 19:11:04 -0700

    Added toggle between encrypt and decrypt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
-const resultArea = document.getElementById("res");
-const equationArea = document.getElementById("eqn");
+const encryptOrDecrypt = ["e"];
 const plainEntry = document.getElementById("usr-plain");
 const shiftEntry = document.getElementById("usr-shift");
-const encryptBtn = document.getElementById("btn-encrypt");
+const resultArea = document.getElementById("res");
+const equationArea = document.getElementById("eqn");
+const screenArea = document.getElementById("screen");
 
 const alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
@@ -12,14 +13,49 @@ const onlyNumberEntry = () =>  {
     shiftEntry.value = shiftEntry.value.replace(/\D/g, "");
 }
 
+const toggleEnDecrypt = () => {
+    encryptOrDecrypt[0] = encryptOrDecrypt[0] == "e" ? "d" : "e";
+}
+
+const displayScreen = () => {
+    let thisVal = encryptOrDecrypt[0] == "e" ? "encrypt" : "decrypt";
+
+    screenArea.innerHTML = `
+        <button id="btn-${thisVal}" disabled>${thisVal.toUpperCase()}!</button>
+    `;
+
+    plainEntry.addEventListener("change", () => {
+        if (shiftEntry.value && plainEntry.value) {
+            updateEquation();
+        }
+    })
+
+    shiftEntry.addEventListener("change", () => {
+        onlyNumberEntry();
+        if (shiftEntry.value && plainEntry.value) {
+            updateEquation();
+        }
+    });
+
+    if (encryptOrDecrypt[0] == "e") {
+        document.getElementById("btn-encrypt").addEventListener("click", encryptText);
+    } else {
+        document.getElementById("btn-decrypt").addEventListener("click", decryptText);
+    }
+}
+
 const updateEquation = () => {
+    let thisButton = encryptOrDecrypt[0] == "e" ? document.getElementById("btn-encrypt"): document.getElementById("btn-decrypt");
+
+    let thisSymbol = encryptOrDecrypt[0] == "e" ? "+" : "-";
+
     let shiftBy = shiftEntry.value;
     let plaintext = plainEntry.value;
     equationArea.innerHTML = `
-        <h3>e(p) = (p + ${shiftBy}) mod 26</h3>
+        <h3>e(p) = (p ${thisSymbol} ${shiftBy}) mod 26</h3>
         <h5>p = ${plaintext}</h5>
     `;
-    encryptBtn.disabled = false;
+    thisButton.disabled = false;
 }
 
 const encryptText = () => {
@@ -46,15 +82,28 @@ const encryptText = () => {
     shiftEntry.value = ""
 }
 
-plainEntry.addEventListener("change", () => {
-    if (shiftEntry.value && plainEntry.value) {
-        updateEquation();
-    }
-})
-shiftEntry.addEventListener("change", () => {
-    onlyNumberEntry();
-    if (shiftEntry.value && plainEntry.value) {
-        updateEquation();
-    }
-});
-encryptBtn.addEventListener("click", encryptText);
+const decryptText = () => {
+    let shiftBy = parseInt(shiftEntry.value);
+    let textToEncrypt = plainEntry.value.toUpperCase().split('')
+    let nextStep = textToEncrypt.map((p)=> p == " " ? " " : (alpha.indexOf(p) - shiftBy) % 26);
+    let shiftedCode = nextStep.map((p)=> p == " " ? " " : alpha[p]).join("");
+    resultArea.innerHTML = `
+    <div>
+        <div>
+            <b>Plain text:</b> ${plainEntry.value}
+        </div>
+        <div>
+            <b>Shift By:</b> ${shiftBy}
+        </div>
+    </div>
+    <div>
+        <div>
+            <b>Code:</b> ${shiftedCode}
+        </div>
+    </div>
+    `
+    plainEntry.value = ""
+    shiftEntry.value = ""
+}
+
+displayScreen()
